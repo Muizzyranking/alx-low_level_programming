122731f793c1eba79de6b999af6121f26d70eb9e
Muizzyranking <oyebowaleabdulmuiz@gmail.com>
2023-09-07 19:30:38 +0100

    new c program that prints some character

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - This is where the program starts
+ *
+ * Return: the return value is 0 (sucess)
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
