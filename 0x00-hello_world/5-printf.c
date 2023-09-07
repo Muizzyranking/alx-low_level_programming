e5b362131c472fc6e1f9f9fda6e4f41a213054f0
Muizzyranking <oyebowaleabdulmuiz@gmail.com>
2023-09-07 19:34:06 +0100

    program that prints a string.

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - the start of the program
+ *
+ * Return: the return value is 0 sucess
+ */
+
+int main(void)
+{
+	printf("ith proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
