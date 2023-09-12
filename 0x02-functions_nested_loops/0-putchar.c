8a209a2655f8dfce229a41c7065c65685cc390b0
Muizzyranking <oyebowaleabdulmuiz@gmail.com>
2023-09-12 23:24:01 +0100

    add all files

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,18 +1,16 @@
 #include "main.h"
-
 /**
- * main - prints _putchar and a new line;
- * Return: returns 0
- */
+* main - Entry point of the program
+* Return: 0 success
+*/
 
 int main(void)
 {
-	int i;
 	char str[] = "_putchar";
+	int i;
 
-	for (i = 0; i <= 8; i++)
+	for (i = 0; i < 8; i++)
 		_putchar(str[i]);
 	_putchar('\n');
-
 	return (0);
 }
--- 0x02-functions_nested_loops/README.md
+++ 0x02-functions_nested_loops/README.md
@@ -1,1 +1,1 @@
-# README
+A readme
