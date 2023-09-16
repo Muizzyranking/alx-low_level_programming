3ef22883a7d1d7346fc0af3ba8b532fc0e8ab73e
Muizzyranking <oyebowaleabdulmuiz@gmail.com>
2023-09-16 17:35:06 +0100

    prints most numbers

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - prints number 0 to 9 with a new line
+ *			should not print 2 and 4
+ */
+
+void print_most_numbers(void)
+{
+
+	int num;
+
+	for (num = 0; num <= 9; num++)
+	{
+		if (num == 2 || num == 4)
+			continue
+		_putchar(num + 48);
+	}
+	_putchar('\n');
+}
