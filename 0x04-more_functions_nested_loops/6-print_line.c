f0bad6a61c292574781138998d94be167887aad9
Muizzyranking <oyebowaleabdulmuiz@gmail.com>
2023-09-18 18:34:49 +0100

    print line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_line - prints a straight line to the terminal
+ *
+ * @n: the  number of times _ will be printed
+ */
+
+void print_line(int n)
+{
+	if (n <= 0)
+		_putchar ('\n');
+	while (n)
+	{
+		_putchar('_');
+		n--;
+	}
+	_putchar ('\n');
+}
