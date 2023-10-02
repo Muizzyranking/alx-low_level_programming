a59898de4ab511553dd192ac79e812968b76068e
Muizzyranking <oyebowaleabdulmuiz@gmail.com>
2023-10-03 00:00:27 +0100

    0 whatsmyname

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - a program that prints its name followed by a new line
+ * @argc: the number of arguments passed to the program
+ * @argv: array of arguments
+ *
+ * Return: 0
+ */
+
+
+int main(int __attribute__((__unused__)) argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
