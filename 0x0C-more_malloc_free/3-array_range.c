1862d4be466bb6be0e718ea2a1f6174f68e206d9
muizzyranking <oyebowaleabdulmuiz@gmail.com>
2023-10-16 00:19:41 +0100

    array range

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -19,11 +19,10 @@ int *array_range(int min, int max)
 
 	size = max - min + 1;
 
-	arr = malloc (sizeof(int) * size);
+	arr = malloc(sizeof(int) * size);
 
 	if (arr == NULL)
 		return (NULL);
-	
 	for (i = 0; i < size; i++)
 	{
 		arr[i] = min;
