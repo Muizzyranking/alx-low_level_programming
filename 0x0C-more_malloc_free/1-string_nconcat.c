e6e8fd16d891648522e34fd9d99480bdf77af4d8
muizzyranking <oyebowaleabdulmuiz@gmail.com>
2023-10-15 21:12:58 +0100

    string nconcat

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,46 @@
+#include "main.h"
+
+/**
+* string_nconcat - concatenates a string using at most @n bytes
+* @s1: the first string
+* @s2: the second string
+* @n: max number of bytes of s2 to be concatenated to s1
+* Return: a pointer to the concatenated space in memory
+*         Null if failed
+*
+*/
+
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	char *concat;
+	unsigned int len = n, i;
+
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+	for (i = 0; s[i]; i++)
+		len++;
+
+	concat = malloc(sizeof(char) * (len + 1));
+
+	if (concat == NULL)
+		return (NULL);
+
+	len = 0;
+	
+	for (i = 0; s1[i]; i++)
+	{
+		concat[len] = s[i];
+		len++;
+	}
+	for (i = 0; s2[i] && i < n; i++)
+	{
+		concat[len] = s2[i];
+		len++;
+	}
+	concat[len] = '\0';
+
+	return (concat);
+
+}
--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -2,7 +2,9 @@
 #define MAIN_H
 
 #include <stdio.h>
+#include <stdlib.h>
 
 int _putchar(char c);
 void *malloc_checked(unsigned int b);
+char *string_nconcat(char *s1, char *s2, unsigned int n);
 #endif /* MAIN_H */
