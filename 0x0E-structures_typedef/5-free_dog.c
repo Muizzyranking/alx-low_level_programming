14422c429cff457dab4e8e4ec0361a5408bd6257
Muizzyranking <oyebowaleabdulmuiz@gmail.com>
2023-10-29 16:04:53 +0100

    free dog

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * free_dog - frees dog
+ * @d: pointer to the dog to be freed
+*/
+
+void free_dog(dog_t *d)
+{
+	if (d == NULL)
+		return;
+
+	free(d->name);
+	free(d->owner);
+	free(d);
+}
