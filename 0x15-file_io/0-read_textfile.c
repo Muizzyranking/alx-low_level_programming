18b8911c9e1c2debf5802128a62a8dd568820449
Muizzyranking <oyebowaleabdulmuiz@gmail.com>
2023-11-21 18:01:56 +0100

    added readme

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,5 +1,13 @@
 #include "main.h"
 
+/*
+* read_textfile - reads a text file and prints it to the POSIX standard output.
+* @filename: name of the file to read.
+* @letters: number of letters to read and print.
+* Return: number of letters read and printed, 0 if failed.
+*/
+
+
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 	int fd, r, w;
