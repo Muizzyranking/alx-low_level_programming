da369d98bdd5f06237d8eec20c1fa306ce523a52
Muizzyranking <oyebowaleabdulmuiz@gmail.com>
2023-11-23 22:57:12 +0100

    read text files
    functions that read a specific lenght of a function and prints it.

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,39 +1,38 @@
 #include "main.h"
 
-/*
-* read_textfile - reads a text file and prints it to the POSIX standard output.
-* @filename: name of the file to read.
-* @letters: number of letters to read and print.
-* Return: number of letters read and printed, 0 if failed.
+/**
+* read_textfile - reads a text file and prints it to the POSIX standard output
+* @filename: name of the file to read
+* @letters: number of letters to read and print
+* Return: number of letters read and printed, 0 if failed
 */
 
 
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int fd, r, w;
-	char *buf;
+	 ssize_t o, r, w;
+	char *buffer;
 
-	if (!filename)
+	if (filename == NULL)
 		return (0);
-	buf = malloc(sizeof(char) * letters);
-	if (!buf)
+
+	buffer = malloc(sizeof(char) * letters);
+	if (buffer ==  NULL)
 		return (0);
-	fd = open(filename, O_RDONLY);
-	if (fd == -1){
-		free(buf);
-		return (0);
-	}
-	r = read(fd, buf, letters);
-	if (r == -1){
-		free(buf);
-		return (0);
-	}
-	w = write(STDOUT_FILENO, buf, r);
-	if (w == -1){
-		free(buf);
+
+	o = open(filename, O_RDONLY);
+	r = read(o, buffer, letters);
+	w = write(STDOUT_FILENO, buffer, r);
+
+	if (o == -1 || r == -1 || w == -1 || w != r)
+	{
+		free(buffer);
 		return (0);
 	}
-	close(fd);
-	free(buf);
+
+	free(buffer);
+	close(o);
+
 	return (w);
+
 }
