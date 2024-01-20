d474b2091cf402e85a990cc22876818a91aa7942
muizzyranking <oyebowaleabdulmuiz@gmail.com>
2024-01-21 00:56:20 +0100

    free list

---
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,15 @@
+#include "lists.h"
+
+/**
+ * free_dlistint - function that frees a dlistint_t list
+ * @head: pointer to head of list
+ *
+*/
+
+void free_dlistint(dlistint_t *head)
+{
+	if (head == NULL)
+                return;
+        free_dlistint(head->next);
+        free(head);
+}
