f17fd98cbf0ea372cbfcc9f83cee8689786ccaa4
muizzyranking <oyebowaleabdulmuiz@gmail.com>
2024-01-20 19:42:33 +0100

    fixed betty issues

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -2,22 +2,26 @@
 
 /**
  * add_dnodeint - adds a new node at the beginning of a dlistint_t list
+ * @head: pointer to head of list
+ * @n: data for new node
+ * Return: address of new element or NULL if failed
 */
 
 
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
-        dlistint_t *new = malloc(sizeof(dlistint_t));
+	dlistint_t *new = malloc(sizeof(dlistint_t));
+
 	if (new == NULL)
-                return NULL;
+		return (NULL);
 
 	new->n = n;
 	new->next = *head;
-        new->prev = NULL;
+	new->prev = NULL;
 
 	if (*head != NULL)
-		( * head)->prev = new;
+		(*head)->prev = new;
 	*head = new;
 
-	return new;
+	return (new);
 }
