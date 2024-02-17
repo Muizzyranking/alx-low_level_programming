47c1da39a2036ce052cee6a13fc07149db758f73
muizzyranking <oyebowaleabdulmuiz@gmail.com>
2024-02-17 16:40:27 +0100

    refactoredd

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,79 +1,83 @@
-#include"dog.h"
+#include "dog.h"
+#include <stdlib.h>
 
-int _len(char *str);
-char *_strcpy(char *dest, char *src);
-
-/**
- * new_dog - creates a new dog
- * @name: name of the dog
- * @age: age of the dog
- * @owner: owner of the dog
- * Return: returns the new dog
-*/
-
-dog_t *new_dog(char *name, float age, char *owner)
-{
-	dog_t *dog;
-
-	if (name == NULL || age < 0 || owner == NULL)
-		return (NULL);
-
-
-	dog = malloc(sizeof(dog_t));
-	if (dog == NULL)
-		return (NULL);
-
-
-	dog->name = malloc(sizeof(char) * (_len(name)));
-	dog->owner = malloc(sizeof(char) * (_len(owner)));
-
-	if (dog->name == NULL)
-	{
-		free(dog);
-		return (NULL);
-	}
-	if (dog->owner == NULL)
-	{
-		free(dog);
-		return (NULL);
-	}
-
-	dog->name = _strcpy(dog->name, name);
-	dog->age = age;
-	dog->owner = _strcpy(dog->owner, owner);
-
-	return (dog);
-
-}
+int _strlen(char *str);
+char *_strcopy(char *dest, char *src);
+dog_t *new_dog(char *name, float age, char *owner);
 
 /**
- * _len - checks the lenght of a string + '\0'
- * @str: string to be checked
- * Return: returns the lenght + 1 of the string
-*/
-
-int _len(char *str)
+ * _strlen - Finds the length of a string.
+ * @str: The string to be measured.
+ *
+ * Return: The length of the string.
+ */
+int _strlen(char *str)
 {
-	int len = 1;
+	int len = 0;
 
-	while (str)
+	while (*str++)
 		len++;
+
 	return (len);
 }
 
 /**
- *_strcpy - copy string pointed by src into dest variable
-* @dest:buffer storing string copy
-* @src: buffer storing string to copy
-* Return:returns copied string
-*/
-char *_strcpy(char *dest, char *src)
+ * _strcopy - Copies a string pointed to by src, including the
+ *            terminating null byte, to a buffer pointed to by dest.
+ * @dest: The buffer storing the string copy.
+ * @src: The source string.
+ *
+ * Return: The pointer to dest.
+ */
+char *_strcopy(char *dest, char *src)
 {
 	int index = 0;
 
-	while (src[index++])
+	for (index = 0; src[index]; index++)
 		dest[index] = src[index];
 
 	dest[index] = '\0';
+
 	return (dest);
+}
+
+/**
+ * new_dog - Creates a new dog.
+ * @name: The name of the dog.
+ * @age: The age of the dog.
+ * @owner: The owner of the dog.
+ *
+ * Return: The new struct dog.
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *doggo;
+
+	if (name == NULL || age < 0 || owner == NULL)
+		return (NULL);
+
+	doggo = malloc(sizeof(dog_t));
+	if (doggo == NULL)
+		return (NULL);
+
+	doggo->name = malloc(sizeof(char) * (_strlen(name) + 1));
+	if (doggo->name == NULL)
+	{
+		free(doggo);
+		return (NULL);
+	}
+
+	doggo->owner = malloc(sizeof(char) * (_strlen(owner) + 1));
+	if (doggo->owner == NULL)
+	{
+		free(doggo->name);
+		free(doggo);
+		return (NULL);
+	}
+
+	doggo->name = _strcopy(doggo->name, name);
+	doggo->age = age;
+	doggo->owner = _strcopy(doggo->owner, owner);
+
+	return (doggo);
 }
