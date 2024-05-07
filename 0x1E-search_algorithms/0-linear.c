5d5643feba55a4f3e88fff4d27469cfd5cd42579
muizzyranking <oyebowaleabdulmuiz@gmail.com>
2024-05-07 19:04:12 +0100

    linear search mod

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -12,16 +12,15 @@
 int linear_search(int *array, size_t size, int value)
 {
 	size_t i;
-	int ret = -1;
 
 	if (array == NULL)
-		return (ret);
+		return (-1);
 
 	for (i = 0; i < size; i++)
 	{
 		printf("Value checked array[%lu] = [%d]\n", i, array[i]);
 		if (array[i] == value)
-			ret = i;
+			return (i);
 	}
-	return (ret);
+	return -1;
 }
