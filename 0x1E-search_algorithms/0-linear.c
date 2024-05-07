be21175a51a24addd355218ce9adb7e4d8f66018
muizzyranking <oyebowaleabdulmuiz@gmail.com>
2024-05-07 19:01:05 +0100

    linear search

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -14,11 +14,14 @@ int linear_search(int *array, size_t size, int value)
 	size_t i;
 	int ret = -1;
 
+	if (array == NULL)
+		return (ret);
+
 	for (i = 0; i < size; i++)
 	{
 		printf("Value checked array[%lu] = [%d]\n", i, array[i]);
 		if (array[i] == value)
-			ret = array[i];
+			ret = i;
 	}
 	return (ret);
 }
