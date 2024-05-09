2607200d86ac04d2bf9db94700367ae36964133c
muizzyranking <oyebowaleabdulmuiz@gmail.com>
2024-05-09 02:57:17 +0100

    interpolition search

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -14,5 +14,6 @@ int search_block(int *array, size_t start, size_t end, size_t size, int value);
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
 int jump_search(int *array, size_t size, int value);
+int interpolation_search(int *array, size_t size, int value);
 
 #endif /* SEARCH_ALGOS_H */
