a676ec3fb180114615546ce6ae874bf974ec57a7
muizzyranking <oyebowaleabdulmuiz@gmail.com>
2024-05-09 03:15:18 +0100

    jump searchg

---
+++ 0x1E-search_algorithms/105-jump_list.c
@@ -0,0 +1,99 @@
+#include "search_algos.h"
+
+/**
+ * get_element_by_index - Searches for a node in a list using its index
+ * @list: Is a pointer to the head of the list to search in
+ * @index: Is the index to find its node
+ * Return: The node at the passed index
+*/
+
+listint_t *get_element_by_index(listint_t *list, size_t index)
+{
+	listint_t *result = NULL;
+
+	if (!list)
+		return (result);
+
+	while (list)
+	{
+		if (list->index == index)
+		{
+			result = list;
+			break;
+		}
+		list = list->next;
+	}
+	return (result);
+}
+
+/**
+ * search_blk - Searches for a value in a part of a list of integers
+ * @list: Is a pointer to the head of the list to search in
+ * @end: Is the index to stop searching at
+ * @value: Is the value to search for
+ * Return: The first index where value is located
+ */
+
+listint_t *search_blk(listint_t *list, size_t end, int value)
+{
+	listint_t *result = NULL;
+
+	printf("Value found between indexes [%ld] and [%ld]\n", list->index, end);
+	while (list)
+	{
+		printf("Value checked at index [%ld] = [%d]\n", list->index, list->n);
+
+		if (list->n == value)
+		{
+			result = list;
+			break;
+		}
+		list = list->next;
+	}
+	return (result);
+}
+
+/**
+ * jump_list - Searches for a value in a sorted list of integers
+ * using the Jump search algorithm.
+ *
+ * @list: Is a pointer to the head of the list to search in
+ * @size: Is the number of nodes in list
+ * @value: Is the value to search for
+ * Return: A pointer to the first node where value is located
+*/
+
+listint_t *jump_list(listint_t *list, size_t size, int value)
+{
+	listint_t *result = NULL, *prev, *cur = list;
+	size_t jump_step = sqrt(size), block_start = 0, block_end = jump_step;
+	size_t block_last_index = 0;
+
+	if (!list)
+		return (result);
+
+	for (block_start = 0; block_start < size; block_start += jump_step)
+	{
+		block_last_index = block_end < size ? block_end : size - 1;
+		prev = cur;
+		cur = get_element_by_index(prev, block_last_index);
+		printf("Value checked at index [%ld] = [%d]\n", cur->index, cur->n);
+		if (block_end < size)
+		{
+			if (cur->n >= value)
+			{
+				result = search_blk(prev, block_last_index, value);
+				break;
+			}
+		}
+		else
+		{
+			result = search_blk(prev, block_last_index, value);
+			break;
+		}
+		block_end += jump_step;
+	}
+
+	return (result);
+}
+
--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -1,16 +1,33 @@
 #ifndef SEARCH_ALGOS_H
 #define SEARCH_ALGOS_H
 
+#include <math.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <stdio.h>
-#include <math.h>
+
+/**
+ * struct listint_s - singly linked list
+ *
+ * @n: Integer
+ * @index: Index of the node in the list
+ * @next: Pointer to the next node
+ *
+ * Description: singly linked list node structure
+ */
+typedef struct listint_s
+{
+	int n;
+	size_t index;
+	struct listint_s *next;
+} listint_t;
 
 /* helper functions */
 void print_array(int *array, size_t start, size_t end);
 int search_block(int *array, size_t start, size_t end, size_t size, int value);
 int binary_recursive(int *array, size_t start, size_t end, int value);
-
+listint_t *get_element_by_index(listint_t *list, size_t index);
+listint_t *search_blk(listint_t *list, size_t end, int value);
 
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
@@ -18,5 +35,6 @@ int jump_search(int *array, size_t size, int value);
 int interpolation_search(int *array, size_t size, int value);
 int exponential_search(int *array, size_t size, int value);
 int advanced_binary(int *array, size_t size, int value);
+listint_t *jump_list(listint_t *list, size_t size, int value);
 
 #endif /* SEARCH_ALGOS_H */
